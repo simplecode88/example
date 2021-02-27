008a4bdb6d3e2d99ded14288977ee937eb921496
samoylova.ana <samoylova.ana@gmail.com>
2021-02-27 16:06:51 +0300

    Hi bro

---
+++ Files/domzd/domzd.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int wordsf = 0;
+	string nfile;
+	cout << "Enter the file name: \n";
+	cin >> nfile;
+	ifstream f;
+	f.open(nfile, ios::in);
+
+	if(!f.is_open())
+		cout << "There is no file in the directory, error\n";
+    else
+    {
+        cout << "File is open" << endl;
+        string s;
+
+        while(1)
+        {
+            getline(f, s, ' ');
+            if (f.eof())
+                break;
+            wordsf++;
+        }
+
+        cout << "Words n: " << wordsf;
+    }
+
+    return 0;
+}
